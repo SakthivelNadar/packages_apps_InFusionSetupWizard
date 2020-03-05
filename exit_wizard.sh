9fe982e3af284aee63f27bbb8f083a1140792b70
SparXFusion <s2234nadar@gmail.com>
2020-03-05 09:53:03 +0530

    Bringup InFusionX-OS

--- exit_wizard.sh
+++ exit_wizard.sh
@@ -2,12 +2,12 @@
 
 adb root
 wait ${!}
-adb shell pm enable org.carbonrom.setupwizard/org.carbonrom.setupwizard.SetupWizardExitActivity || true
+adb shell pm enable org.infusion.setupwizard/org.carbonrom.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 adb shell pm enable com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 sleep 1
-adb shell am start org.carbonrom.setupwizard/org.carbonrom.setupwizard.SetupWizardExitActivity || true
+adb shell am start org.infusion.setupwizard/org.carbonrom.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 sleep 1
 adb shell am start com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity
